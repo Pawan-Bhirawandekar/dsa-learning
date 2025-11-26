d494ea71560c7e3a4af0a6063887715fa516addc
Pawan Bhirawandekar <pawanbhirawandekar@gmail.com>
2025-11-26 20:53:47 +0530

    implement the deletion operation in queue

--- Array/Queue/queue_opertaions.c
+++ Array/Queue/queue_opertaions.c
@@ -1,63 +1,68 @@
-#include <stdio.h>
+#include<stdio.h>
 #define SIZE 5
 int queue[SIZE];
-int front = -1, rear = -1, val;
-void insertion(int value)
-{
-    if (rear == SIZE - 1)
-    {
+int front=-1,rear=-1,val;
+void insertion(int value){
+    if(rear==SIZE-1){
         printf("Queue is full\n");
         return;
     }
-    if (front == -1)
-    {
-        front = 0;
+    if(front==-1){
+        front=0;
     }
     rear++;
-    queue[rear] = value;
-    printf("%d inserted inside the queue\n", value);
+    queue[rear]=value;
+    printf("%d inserted inside the queue\n",value);
 }
 
-void display()
-{
-    if (front == -1 || front > rear)
-    {
+void deletion(){
+    if(front==-1){
         printf("Queue is empty\n");
         return;
     }
-
-    for (int i = front; i <= rear; i++)
-    {
-        printf("%d\n", queue[i]);
+    printf("%d element deleted successfully\n",queue[front]);
+    front++;
+    if(front>rear){
+        front=rear=-1;
     }
 }
 
-int main()
-{
+void display(){
+    if(front==-1||front>rear){
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue elements are:\n");
+    for(int i=front;i<=rear;i++){
+        printf("%d\n",queue[i]);
+    }
+}
+
+int main(){
     int opt;
-    while (1)
-    {
+    while(1){
         printf("\n*****Queue Operations*****");
-        printf("\n1.Insertion \n2.Display \n3.Exit \n");
+        printf("\n1.Insertion \n2.Display \n3.Deletion \n4.Exit \n");
         printf("Choose an option: ");
-        scanf("%d", &opt);
-        switch (opt)
-        {
-        case 1:
-            printf("\nEnter an element: ");
-            scanf("%d", &val);
-            insertion(val);
-            break;
-        case 2:
-            display();
-            break;
-        case 3:
-            printf("****Exiting the program*******");
-            return 0;
-            break;
-        default:
-            printf("Choose the valid option.");
+        scanf("%d",&opt);
+        switch(opt){
+            case 1:
+                printf("\nEnter an element: ");
+                scanf("%d",&val);
+                insertion(val);
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                deletion();
+                break;
+            case 4:
+                printf("****Exiting the program*******");
+                return 0;
+            default:
+                printf("Choose the valid option.\n");
         }
     }
-    return 0;
+
 }
