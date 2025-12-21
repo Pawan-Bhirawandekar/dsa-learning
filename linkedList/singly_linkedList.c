9be8d4e4742f1f7b5ea82fa957802082ef414a5a
Pawan Bhirawandekar <pawanbhirawandekar@gmail.com>
2025-12-21 20:44:48 +0530

    add insertion at the specific position operation

--- linkedList/singly_linkedList.c
+++ linkedList/singly_linkedList.c
@@ -7,9 +7,8 @@ struct Node{
 };
 
 struct Node *head=NULL;
-
-//Insertion at the End
-void insertEnd(int value){
+//insert at the END
+void insertionEnd(int value){
     struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
     newNode->data=value;
     newNode->next=NULL;
@@ -23,56 +22,105 @@ void insertEnd(int value){
         }
         temp->next=newNode;
     }
-    printf("Element inserted at the End successfully.\n");
+    
+    printf("\n%d node successfully inserted at the end of singly linked list.\n",value);
 }
 
-//Insertion at the Begining
+//insert at the Begining
 void insertBegin(int value){
     struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
     newNode->data=value;
     newNode->next=head;
     head=newNode;
-    printf("Element inserted at the Begining successfully.\n");
+    printf("%d element successfully insrted at the begining of the singly linked list.\n",value);
 }
 
-//Display the linked list
-void display(){
-    struct Node *temp=head;
-    if(temp==NULL){
-        printf("List is empty.\n");
+void insertAtPosition(int value,int pos){
+    int i=1;
+    
+    if(pos<=0){
+        printf("Please eneter valid position.");
         return;
     }
-    printf("Linked list: \n");
-    while(temp!=NULL){
-        printf("%d ->",temp->data);
-        temp=temp->next;
+    
+    struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=value;
+    newNode->next=NULL;
+    
+    if(pos==1){
+        newNode->next=head;
+        head=newNode;
+        printf("%d Element inserted successfully.",value);
+        return;
     }
-    printf("NULL\n");
+    
+    if(head==NULL){
+            printf("Position out of bound.");
+            free(newNode);
+            return;
+        }
+        
+        struct Node *temp=head;
+        
+        for (i = 1; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->next;
+    }
+        if (temp == NULL) {
+        printf("Position out of bound.\n");
+        free(newNode);
+        return;
+    }
+        
+        newNode->next=temp->next;
+        temp->next=newNode;
+        
+        printf("%d Element successfully insrted at %d position.",value,pos);
+        
 }
 
-int main(){
-    int opt,value;
-    while(1){
-        printf("Singly Linked List:");
-        printf("\n1.Insertion at the Begining of Linked List \n2.Insertion at the End of Singly Linked List\n3.Display\n4.Exit\n");
-        printf("\nEnter your choice:");
-        scanf("%d",&opt);
-        switch(opt){
-            case 1: printf("Enter a value: ");
-                    scanf("%d",&value);
-                    insertBegin(value);
-                    break;
-            case 2: printf("Enter a value: ");
-                    scanf("%d",&value);
-                    insertEnd(value);
-                    break;
-            case 3: display();
-                    break;
-            case 4: printf("Exiting....");
-                    return 0;
-                    break;
-            default : printf("Enter valid choice.");
+void display(){
+    struct Node *temp=head;
+    if(head==NULL){
+        printf("List is empty.");
+        return;
     }
+    printf("Singly Linked List Contains: ");
+    while(temp!=NULL){
+        printf("%d -> ",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+}
+
+
+int main(){
+    int ch,val,pos;
+    printf("\nSingly Linked List: ");
+    while(1){
+        printf("\n1.Insert element at the End.  \n2.Insert element at the Begining. \n3.Insert an element at specific position.  \n4.Display. \n5.Exit. \nEnter your choice:");
+        scanf("%d",&ch);
+        switch(ch){
+        case 1: printf("Enter an element: ");
+        scanf("%d",&val);
+        insertionEnd(val);
+        break;
+        case 2: printf("Enter an element: ");
+        scanf("%d",&val);
+        insertBegin(val);
+        break;
+        case 3: printf("Enter position where you want to insert an element: ");
+        scanf("%d",&pos);
+        printf("Enter an element: ");
+        scanf("%d",&val);
+        insertAtPosition(val,pos);
+        break;
+        case 4: display();
+        break;
+        case 5: printf("Exiting the program.....");
+        return 0;
+        break;
+        default: printf("Enter valid choice. ");
+        }
     }
     return 0;
 }
